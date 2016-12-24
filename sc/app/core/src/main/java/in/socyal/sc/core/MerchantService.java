144feda20b7fd427a4f4743a660d2d7430d174ff
Akash Nigam <akashnigam010@gmail.com>
2016-12-24 19:22:50 +0530

    Added temporary logging statements for Merchant Service

--- sc/app/core/src/main/java/in/socyal/sc/core/MerchantService.java
+++ sc/app/core/src/main/java/in/socyal/sc/core/MerchantService.java
@@ -18,17 +18,24 @@ import in.socyal.sc.app.merchant.MerchantDelegate;
 import in.socyal.sc.core.mapper.MerchantServiceMapper;
 import in.socyal.sc.helper.ResponseHelper;
 import in.socyal.sc.helper.exception.BusinessException;
+import in.socyal.sc.helper.logging.LogManager;
+import in.socyal.sc.helper.logging.Logger;
 
 @RestController
 @RequestMapping(value = "/merchant")
 public class MerchantService {
+	//Logger LOG = LogManager.getLogger(MerchantService.class);
 
-	@Autowired MerchantDelegate delegate;
-	@Autowired MerchantServiceMapper mapper;
-	@Autowired ResponseHelper responseHelper;
-	
+	@Autowired
+	MerchantDelegate delegate;
+	@Autowired
+	MerchantServiceMapper mapper;
+	@Autowired
+	ResponseHelper responseHelper;
+
 	@RequestMapping(value = "/getMerchants", method = RequestMethod.POST, headers = "Accept=application/json")
 	public GetMerchantListResponse getMerchants(@RequestBody GetMerchantListRequest request) {
+		logGetMerchantRequest(request);
 		GetMerchantListResponse response = new GetMerchantListResponse();
 		try {
 			response = delegate.getMerchants(request);
@@ -41,31 +48,30 @@ public class MerchantService {
 
 	@RequestMapping(value = "/getMerchantDetails", method = RequestMethod.POST, headers = "Accept=application/json")
 	public MerchantDetailsResponse getMerchantDetails(@RequestBody MerchantDetailsRequest request) {
-		//Mock data is present in mapper
-		//MerchantDetailsResponse response = mapper.mapMerchantDetails();
+		// Mock data is present in mapper
+		// MerchantDetailsResponse response = mapper.mapMerchantDetails();
 		MerchantDetailsResponse response = new MerchantDetailsResponse();
 		try {
 			response = delegate.getMerchantDetails(request);
 			return responseHelper.success(response);
 		} catch (BusinessException e) {
 			return responseHelper.failure(response, e);
-		} 
+		}
 	}
 
 	@RequestMapping(value = "/searchMerchant", method = RequestMethod.POST, headers = "Accept=application/json")
 	public SearchMerchantResponse searchMerchant(@RequestBody SearchMerchantRequest request) {
-		//Mock data is present in mapper
-		//SearchMerchantResponse response = mapper.mapSearchMerchantResponse();
+		logSearchMerchantRequest(request);
 		SearchMerchantResponse response = new SearchMerchantResponse();
 		try {
 			response = delegate.searchMerchant(request);
 			return responseHelper.success(response);
 		} catch (BusinessException e) {
 			return responseHelper.failure(response, e);
-		} 
+		}
 	}
-	
-	//Testing purpose
+
+	// Testing purpose
 	@RequestMapping(value = "/saveMerchantDetails", method = RequestMethod.POST, headers = "Accept=application/json")
 	public SaveMerchantDetailsResponse saveMerchantDetails(@RequestBody SaveMerchantDetailsRequest request) {
 		SaveMerchantDetailsResponse response = new SaveMerchantDetailsResponse();
@@ -74,7 +80,21 @@ public class MerchantService {
 			return responseHelper.success(response);
 		} catch (BusinessException e) {
 			return responseHelper.failure(response, e);
-		} 
+		}
 
+	}
+
+	private void logSearchMerchantRequest(SearchMerchantRequest request) {
+		System.out.println("========= SEARCH MERCHANT REQUEST START ============");
+		System.out.println("Search String : " + request.getSearchString());
+		System.out.println("========= SEARCH MERCHANT REQUEST END ============");
+	}
+
+	private void logGetMerchantRequest(GetMerchantListRequest request) {
+		System.out.println("========= GET MERCHANT REQUEST START ============");
+		System.out.println("Latitude : " + request.getLocation().getLatitude().toString());
+		System.out.println("Longitude : " + request.getLocation().getLongitude().toString());
+		System.out.println("Page No : " + request.getPage().toString());
+		System.out.println("========= GET MERCHANT REQUEST END ============");
 	}
 }
