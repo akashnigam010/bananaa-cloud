b90b9e3d4f354e34b55e6694c2fb952ebae2d5ae
Akash Nigam <akashnigam010@gmail.com>
2016-12-27 01:01:16 +0530

    added new service (dummy data) for AROUND ME and MY FEEDS

--- sc/app/core/src/main/java/in/socyal/sc/core/CheckinService.java
+++ sc/app/core/src/main/java/in/socyal/sc/core/CheckinService.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.socyal.sc.api.checkin.dto.CheckinResponseDto;
+import in.socyal.sc.api.checkin.request.AroundMeCheckinsRequest;
 import in.socyal.sc.api.checkin.request.CheckinRequest;
+import in.socyal.sc.api.checkin.request.MyCheckinsRequest;
 import in.socyal.sc.api.checkin.response.CheckinResponse;
 import in.socyal.sc.app.checkin.CheckinDelegate;
 import in.socyal.sc.core.mapper.CheckinServiceMapper;
@@ -26,6 +28,22 @@ public class CheckinService {
 	public CheckinResponse getMerchantCheckins(@RequestBody CheckinRequest request) {
 		CheckinResponse response = new CheckinResponse();
 		List<CheckinResponseDto> checkins = delegate.getRestaurantCheckins(request.getId(), request.getPage());
+		mapper.map(checkins, response, request.getPage());
+		return responseHelper.success(response);
+	}
+	
+	@RequestMapping(value = "/getAroundMeCheckins", method = RequestMethod.POST, headers = "Accept=application/json")
+	public CheckinResponse getAroundMeCheckins(@RequestBody AroundMeCheckinsRequest request) {
+		CheckinResponse response = new CheckinResponse();
+		List<CheckinResponseDto> checkins = delegate.getRestaurantCheckins(123, request.getPage());
+		mapper.map(checkins, response, request.getPage());
+		return responseHelper.success(response);
+	}
+	
+	@RequestMapping(value = "/getMyCheckins", method = RequestMethod.POST, headers = "Accept=application/json")
+	public CheckinResponse getMyCheckins(@RequestBody MyCheckinsRequest request) {
+		CheckinResponse response = new CheckinResponse();
+		List<CheckinResponseDto> checkins = delegate.getRestaurantCheckins(123, request.getPage());
 		mapper.map(checkins, response, request.getPage());
 		return responseHelper.success(response);
 	}
