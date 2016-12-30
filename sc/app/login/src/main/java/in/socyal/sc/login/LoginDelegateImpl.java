1dadda06f85b8de7bb61204c4ecf8252fc3070f0
yogeshsadula <yogeshsadula@cantwait.in>
2016-12-30 22:21:16 +0530

    Code clean up

--- sc/app/login/src/main/java/in/socyal/sc/login/LoginDelegateImpl.java
+++ sc/app/login/src/main/java/in/socyal/sc/login/LoginDelegateImpl.java
@@ -12,7 +12,6 @@ import in.socyal.sc.api.type.RoleType;
 import in.socyal.sc.api.user.dto.UserDto;
 import in.socyal.sc.helper.exception.BusinessException;
 import in.socyal.sc.helper.security.jwt.JwtHelper;
-import in.socyal.sc.login.dao.LoginDao;
 import in.socyal.sc.login.type.LoginErrorCodeType;
 import in.socyal.sc.persistence.UserDao;
 
@@ -24,7 +23,6 @@ public class LoginDelegateImpl implements LoginDelegate {
 	private static final String JWT_LOW_RISK = "ASDJask876ah48y93nLIAUD972OHlijpsojwiejrwjb298273409UAJHDF9P8273YKJHASJNUGILU"
 			+ "Kkugkjgigi787798JKH98Y98YOUHJKKJBIYt876ugG87687GHVGFXDTRe64364eytdyte65RUYFKJBMNLiu98787676RYTCNVJYt76"
 			+ "65E75RFiyg87574y5erytcgvuyt76576";*/
-	@Autowired LoginDao dao;
 	@Autowired UserDao userDao;
 	@Autowired LoginMapper mapper;
 	@Autowired OAuth2FbHelper fbHelper;
