4fdb82deef711306265bb1632da36b6eb4a38db0
yogeshsadula <yogeshsadula@cantwait.in>
2017-01-05 00:48:14 +0530

    Code changes for handling exceptions during spring filter

--- sc/utility/helper/src/main/java/in/socyal/sc/helper/type/GenericErrorCodeType.java
+++ sc/utility/helper/src/main/java/in/socyal/sc/helper/type/GenericErrorCodeType.java
@@ -5,6 +5,7 @@ import in.socyal.sc.helper.BusinessErrorCode;
 public enum GenericErrorCodeType implements BusinessErrorCode {
 	UNKNOWN_HOST_ERROR(90001), 
 	REQUEST_VALIDATION_FAILED(90002),
+	JWT_TOKEN_EXPIRED(90003),
 	GENERIC_ERROR(99999);
 
 	private int value;
