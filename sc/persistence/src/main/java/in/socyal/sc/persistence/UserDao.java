af14127e7777ca43c05d239c416a6ef09fc8fbcf
Akash Nigam <akashnigam010@gmail.com>
2017-01-15 03:31:14 +0530

    added java docs

--- sc/persistence/src/main/java/in/socyal/sc/persistence/UserDao.java
+++ sc/persistence/src/main/java/in/socyal/sc/persistence/UserDao.java
@@ -23,31 +23,33 @@ public class UserDao {
 	private static final String FIRST_NAME = "firstName";
 	private static final String LAST_NAME = "lastName";
 	private static final int RESULTS_PER_PAGE = 15;
-	@Autowired SessionFactory sessionFactory;
-	@Autowired UserDaoMapper mapper;
-	
-   public UserDao() {
-    }
-     
-    public UserDao(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-    
-    public UserDto fetchUser(Integer userId) {
-    	UserDto dto = null;
-    	UserEntity entity = (UserEntity) sessionFactory.getCurrentSession().get(UserEntity.class, userId);
-    	if (entity != null) {
-    		dto = new UserDto();
-    		mapper.map(entity, dto);
-    	}
-    	return dto;
-    }
-    
-    public List<UserDto> fetchUsersBySearchString(String searchString) {
-    	List<UserDto> userDtos = null;
-    	Criteria criteria = sessionFactory.getCurrentSession().createCriteria(UserEntity.class);
-    	Criterion firstNameCriteria = Restrictions.ilike(FIRST_NAME, searchString, MatchMode.ANYWHERE);
-    	Criterion lastNameCriteria = Restrictions.ilike(LAST_NAME, searchString, MatchMode.ANYWHERE);
+	@Autowired
+	SessionFactory sessionFactory;
+	@Autowired
+	UserDaoMapper mapper;
+
+	public UserDao() {
+	}
+
+	public UserDao(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public UserDto fetchUser(Integer userId) {
+		UserDto dto = null;
+		UserEntity entity = (UserEntity) sessionFactory.getCurrentSession().get(UserEntity.class, userId);
+		if (entity != null) {
+			dto = new UserDto();
+			mapper.map(entity, dto);
+		}
+		return dto;
+	}
+
+	public List<UserDto> fetchUsersBySearchString(String searchString) {
+		List<UserDto> userDtos = null;
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(UserEntity.class);
+		Criterion firstNameCriteria = Restrictions.ilike(FIRST_NAME, searchString, MatchMode.ANYWHERE);
+		Criterion lastNameCriteria = Restrictions.ilike(LAST_NAME, searchString, MatchMode.ANYWHERE);
 		criteria.add(Restrictions.or(firstNameCriteria, lastNameCriteria));
 		@SuppressWarnings("unchecked")
 		List<UserEntity> users = (List<UserEntity>) criteria.list();
@@ -55,41 +57,53 @@ public class UserDao {
 			userDtos = new ArrayList<>();
 			mapper.map(users, userDtos);
 		}
-		
+
 		return userDtos;
-    }
-    
-    public List<UserDto> fetchUsersByPage(int page) {
-    	List<UserDto> userDtos = null;
-    	Criteria criteria = sessionFactory.getCurrentSession().createCriteria(UserEntity.class);
-    	int firstResult = ((page - 1) * RESULTS_PER_PAGE);
-    	criteria.setFirstResult(firstResult);
-    	criteria.setMaxResults(RESULTS_PER_PAGE);
+	}
+
+	public List<UserDto> fetchUsersByPage(int page) {
+		List<UserDto> userDtos = null;
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(UserEntity.class);
+		int firstResult = ((page - 1) * RESULTS_PER_PAGE);
+		criteria.setFirstResult(firstResult);
+		criteria.setMaxResults(RESULTS_PER_PAGE);
 		@SuppressWarnings("unchecked")
 		List<UserEntity> users = (List<UserEntity>) criteria.list();
 		if (users != null && !users.isEmpty()) {
 			userDtos = new ArrayList<>();
 			mapper.map(users, userDtos);
 		}
-		
+
 		return userDtos;
-    }
-    
-    public UserDto saveOrUpdate(User user, String fbAccessToken) {
-    	UserDto userDto = new UserDto();
-    	Criteria criteria = sessionFactory.getCurrentSession().createCriteria(UserEntity.class);
-    	criteria.add(Restrictions.eq("facebookId", user.getId()));
-    	UserEntity entity = (UserEntity) criteria.uniqueResult();
-    	
-    	if (entity == null) {
-    		entity = new UserEntity();
-    		mapper.map(user, entity, fbAccessToken);
-    		sessionFactory.getCurrentSession().save(entity);
-    	} else if (StringUtils.equals(entity.getFacebookId(), fbAccessToken)) {
-    		entity.setFacebookId(fbAccessToken);
-    		sessionFactory.getCurrentSession().save(entity);
-    	}
-    	mapper.map(entity, userDto);
-    	return userDto;
-    }
+	}
+
+	/**
+	 * Save or update user details. <br>
+	 * <br>
+	 * If user doesn't exist, a new user is created.<br>
+	 * If user already exists, its FB access token is compared with the passed
+	 * access token, and updated accordingly.<br>
+	 * This is done to update the token if it was expired already
+	 * 
+	 * @param user
+	 * @param fbAccessToken
+	 * @return
+	 */
+	public UserDto saveOrUpdate(User user, String fbAccessToken) {
+		UserDto userDto = new UserDto();
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(UserEntity.class);
+		criteria.add(Restrictions.eq("facebookId", user.getId()));
+		UserEntity entity = (UserEntity) criteria.uniqueResult();
+
+		if (entity == null) {
+			entity = new UserEntity();
+			mapper.map(user, entity, fbAccessToken);
+			sessionFactory.getCurrentSession().save(entity);
+		} else if (StringUtils.equals(entity.getFacebookId(), fbAccessToken)) {
+			entity.setFacebookId(fbAccessToken);
+			sessionFactory.getCurrentSession().save(entity);
+		}
+		mapper.map(entity, userDto);
+		return userDto;
+	}
 }
