c2dd37043967a03a36d05ab9638eb81ce714217a
Akash Nigam <akashnigam010@gmail.com>
2017-01-30 21:57:20 +0530

    Added excitement & app launch modals

--- sc/app/web-ui/src/main/webapp/js/main.js
+++ sc/app/web-ui/src/main/webapp/js/main.js
@@ -26,6 +26,28 @@ $(document).ready(function() {
 		autoResize: true,
 		delayedMode : true
 	});
+    
+    var timerid;    
+    $("#search-field").on("input",function(e){
+        var value = $(this).val();
+        if($(this).data("lastval")!= value){
 
+            $(this).data("lastval",value);        
+            clearTimeout(timerid);
 
+            timerid = setTimeout(function() {
+                //change action
+                $('#excitementModal').modal();
+            },500);
+        };
+    });
+
+    $('#playStoreLink').click(function() {
+    	$('#appLaunchModal').modal();
+    });
+    
+    $('#appStoreLink').click(function() {
+    	$('#appLaunchModal').modal();
+    });
+    
 });
