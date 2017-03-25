35d5cf37d75f3fb559e3ea42c1173f5f8844bdb4
Akash Nigam <akashnigam010@gmail.com>
2017-03-26 04:38:23 +0530

    Added slick slider for trending places

--- sc/app/web-ui/src/main/resources/static/js/main.js
+++ sc/app/web-ui/src/main/resources/static/js/main.js
@@ -81,4 +81,36 @@ $(document).ready(function() {
         }
     });
     
+    $('.slick-class').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 2048,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                slidesToShow: 1.5,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });
+    
 });
