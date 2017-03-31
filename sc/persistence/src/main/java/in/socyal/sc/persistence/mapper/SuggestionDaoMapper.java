5d141ec1fa6b604045f88374b55c913ac834c273
dell <dell@dell-PC>
2017-03-31 18:23:58 +0530

    Added @Component to avoid build failure for last checkin

--- sc/persistence/src/main/java/in/socyal/sc/persistence/mapper/SuggestionDaoMapper.java
+++ sc/persistence/src/main/java/in/socyal/sc/persistence/mapper/SuggestionDaoMapper.java
@@ -1,8 +1,11 @@
 package in.socyal.sc.persistence.mapper;
 
+import org.springframework.stereotype.Component;
+
 import in.socyal.sc.api.suggestion.dto.SuggestionDto;
 import in.socyal.sc.persistence.entity.SuggestionEntity;
 
+@Component
 public class SuggestionDaoMapper {
 
 	public void map(SuggestionEntity entity, SuggestionDto dto) {
