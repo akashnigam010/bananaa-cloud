7d8713816de3259f6d63326616706d2036f797b3
Akash Nigam <akashnigam010@gmail.com>
2017-07-07 07:26:55 +0530

    Home page revamp | initial commit

--- sc/app/web-ui/src/main/resources/static/r/e/s/o/u/r/c/e/js/index.js
+++ sc/app/web-ui/src/main/resources/static/r/e/s/o/u/r/c/e/js/index.js
@@ -9,7 +9,9 @@ $(document).ready(function() {
     }
     
     $('#search-field').typeahead(searchConfig($('#search-field'), $('#search-location')));
+    $('#search-field-add').typeahead(searchConfig($('#search-field-add'), $('#search-location-add')));
     loadLocations($('#search-location'));
+    loadLocations($('#search-location-add'));
     getTrendingRestaurants();
     getStories();
 });
@@ -27,11 +29,16 @@ function getTrendingRestaurants() {
     				  for (var i=0; i<response.merchants.length; i++) {    					  
     					  trendingRestaurantHtml +=
     						  '<div class="flick-div" onclick="javascript:location.href=\''+response.merchants[i].merchantUrl+'\'">'+
-	                              '<img class="flick-image flick-img-txt" src="'+response.merchants[i].thumbnail+'" alt="'+response.merchants[i].name+' at Bananaa" />'+
+	                              '<img class="flick-image flick-img-txt details-wrapper" src="'+response.merchants[i].thumbnail+'" alt="'+response.merchants[i].name+' at Bananaa" />'+
 	                                  '<div class="flick-txt padding-left">'+
-	                                      '<span class="bold font-1-3">'+response.merchants[i].name+'</span>'+
-	                                      '<br />'+
-	                                      '<span class="light font-0-8">'+response.merchants[i].shortAddress+'</span>'+
+	                                      '<div class="pull-left">'+
+	                                        '<span class="font-1-3">'+response.merchants[i].name+'</span>'+
+	                                        '<br />'+
+	                                        '<span class="font-0-8">'+response.merchants[i].shortAddress+'</span>'+    
+	                                      '</div>'+
+	                                      '<div class="pull-right">'+
+//	                                        '<span class="float-right rating-rcmd r25" style="padding: 10% 40%;">'+response.merchants[i].rating+'</span>'+
+	                                      '</div>'+	                                      
 	                                  '</div>'+
 	                              '</div>';
     				  }
@@ -58,7 +65,7 @@ function getStories() {
     				  for (var i=0; i<response.stories.length; i++) {
     					  storiesHtml +=
     						  '<div class="flick-div" onclick="javascript:location.href=\''+response.stories[i].url+'\'">'+
-		                          '<img class="flick-image diary-img" src="'+response.stories[i].imageUrl+'" alt="'+response.stories[i].name+' Story at Bananaa" />'+
+		                          '<img class="flick-image flick-img-txt diary-img details-wrapper" src="'+response.stories[i].imageUrl+'" alt="'+response.stories[i].name+' Story at Bananaa" />'+
 		                          '<div class="align-center padding" style="position: absolute; top: 20%; width: 100%;">'+
 		                              '<p class="bold font-1-3">'+response.stories[i].name+'</p>'+
 		                          '</div>'+
