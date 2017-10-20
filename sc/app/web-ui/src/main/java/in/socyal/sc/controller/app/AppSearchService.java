d413c898105192c2970295ff86c6a54b60b64e11
Akash Nigam <akashnigam010@gmail.com>
2017-10-20 13:48:15 +0530

    Added app- dish search service

--- sc/app/web-ui/src/main/java/in/socyal/sc/controller/app/AppSearchService.java
+++ sc/app/web-ui/src/main/java/in/socyal/sc/controller/app/AppSearchService.java
@@ -2,6 +2,7 @@ package in.socyal.sc.controller.app;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 import in.socyal.sc.api.GenericSearchRequest;
 import in.socyal.sc.api.helper.ResponseHelper;
 import in.socyal.sc.api.helper.exception.BusinessException;
+import in.socyal.sc.api.item.response.SearchItemsResponse;
+import in.socyal.sc.api.merchant.request.SearchRequest;
 import in.socyal.sc.api.merchant.response.GlobalSearchItem;
 import in.socyal.sc.api.merchant.response.GlobalSearchResponse;
 import in.socyal.sc.api.type.TagType;
 import in.socyal.sc.app.merchant.MerchantDelegate;
 import in.socyal.sc.app.rcmdn.ItemDelegate;
-import in.socyal.sc.controller.MerchantService;
+import in.socyal.sc.core.validation.ItemValidator;
 import in.socyal.sc.helper.JsonHelper;
 
 @RestController
@@ -29,11 +32,13 @@ public class AppSearchService {
 	@Autowired
 	ItemDelegate itemDelegate;
 	@Autowired
+	ItemValidator validator;
+	@Autowired
 	ResponseHelper responseHelper;
 
 	@RequestMapping(value = "/gSearch", method = RequestMethod.POST, headers = "Accept=application/json")
 	public GlobalSearchResponse globalSearch(@RequestBody GenericSearchRequest request) {
-		JsonHelper.logRequest(request, MerchantService.class, "/search/globalSearch");
+		JsonHelper.logRequest(request, AppSearchService.class, "/search/globalSearch");
 		GlobalSearchResponse response = new GlobalSearchResponse();
 		try {
 			if (request.getSearchString().length() >= MINIMUM_SEARCH_STRING_LENGTH) {
@@ -43,6 +48,22 @@ public class AppSearchService {
 				response.getSearchItems().addAll(merchants);
 				response.getSearchItems().addAll(cuisines);
 				response.getSearchItems().addAll(suggestions);
+			}
+			return responseHelper.success(response);
+		} catch (BusinessException e) {
+			return responseHelper.failure(response, e);
+		}
+	}
+	
+	@RequestMapping(value = "/dishes", method = RequestMethod.POST, headers = "Accept=application/json")
+	public SearchItemsResponse searchDishes(@RequestBody SearchRequest request) {
+		SearchItemsResponse response = new SearchItemsResponse();
+		try {
+			validator.validateSearchItemsRequest(request);
+			if (StringUtils.isNotEmpty(request.getSearchString())) {
+				if (request.getSearchString().length() >= MINIMUM_SEARCH_STRING_LENGTH) {
+					response = itemDelegate.searchItems(request);
+				}
 			}
 			return responseHelper.success(response);
 		} catch (BusinessException e) {
